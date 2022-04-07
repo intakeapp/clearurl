b11e0dafbf08cbb579608c99bb2e3ff1288e3b37
Wendell Sun <iwendellsun@gmail.com>
2022-04-08 01:08:46 +0800

    implement Clear func

--- clearurl.go
+++ clearurl.go
@@ -3,8 +3,9 @@ package clearurl
 import (
 	_ "embed"
 	"encoding/json"
-	"fmt"
+	"net/url"
 	"regexp"
+	"strings"
 )
 
 //go:embed data.min.json
@@ -19,18 +20,70 @@ func init() {
 }
 
 type Provider struct {
-	Urlpattern       string         `json:"urlPattern"`
-	UrlpatternRegexp *regexp.Regexp `json:"-"`
-	Completeprovider bool           `json:"completeProvider"`
-	Rules            []string       `json:"rules"`
-	Exceptions       []string       `json:"exceptions"`
-	Redirections     []string       `json:"redirections"`
+	Name              string   `json:"-"`
+	UrlPattern        string   `json:"urlPattern"`
+	CompleteProvider  bool     `json:"completeProvider"`
+	Rules             []string `json:"rules"`
+	RawRules          []string `json:"rawRules"`
+	ReferralMarketing []string `json:"referralMarketing"`
+	Exceptions        []string `json:"exceptions"`
+	Redirections      []string `json:"redirections"`
+
+	urlPatternRegexp  *regexp.Regexp   `json:"-"`
+	exceptionsRegexps []*regexp.Regexp `json:"-"`
+	rulesRegexps      []*regexp.Regexp `json:"-"`
+	rawRulesRegexps   []*regexp.Regexp `json:"-"`
 }
 
-func (p *Provider) init() error {
+func (p *Provider) initExceptions() error {
 	var err error
-	if p.UrlpatternRegexp == nil {
-		p.UrlpatternRegexp, err = regexp.Compile(p.Urlpattern)
+	if len(p.Exceptions) > 0 && len(p.exceptionsRegexps) == 0 {
+		exceptionsRegexps := make([]*regexp.Regexp, len(p.Exceptions))
+		for i, e := range p.Exceptions {
+			exceptionsRegexps[i], err = regexp.Compile(e)
+			if err != nil {
+				return err
+			}
+		}
+		p.exceptionsRegexps = exceptionsRegexps
+	}
+	return nil
+}
+
+func (p *Provider) initRules() error {
+	var err error
+	if len(p.Rules) > 0 && len(p.rulesRegexps) == 0 {
+		rulesRegexps := make([]*regexp.Regexp, len(p.Rules))
+		for i, r := range p.Rules {
+			rulesRegexps[i], err = regexp.Compile(r)
+			if err != nil {
+				return err
+			}
+		}
+		p.rulesRegexps = rulesRegexps
+	}
+	return nil
+}
+
+func (p *Provider) initRawRules() error {
+	var err error
+	if len(p.RawRules) > 0 && len(p.rawRulesRegexps) == 0 {
+		rawRulesRegexps := make([]*regexp.Regexp, len(p.RawRules))
+		for i, r := range p.RawRules {
+			rawRulesRegexps[i], err = regexp.Compile(r)
+			if err != nil {
+				return err
+			}
+		}
+		p.rawRulesRegexps = rawRulesRegexps
+	}
+	return nil
+}
+
+func (p *Provider) initUrlPattern() error {
+	var err error
+	if p.urlPatternRegexp == nil {
+		p.urlPatternRegexp, err = regexp.Compile(p.UrlPattern)
 		if err != nil {
 			return err
 		}
@@ -49,8 +102,9 @@ func Init() (*Handler, error) {
 		providers: rulesMap["providers"],
 	}
 
-	for _, p := range h.providers {
-		if err := p.init(); err != nil {
+	for name, p := range h.providers {
+		p.Name = name
+		if err := p.initUrlPattern(); err != nil {
 			return nil, err
 		}
 	}
@@ -61,12 +115,94 @@ func Init() (*Handler, error) {
 	return h, nil
 }
 
-func (h *Handler) Clear(url string) (string, error) {
-	for k, p := range h.providers {
-		if p.UrlpatternRegexp.MatchString(url) {
-			fmt.Println(k, p.Urlpattern)
+func (h *Handler) Preload() error {
+	ps := make([]*Provider, len(h.providers)+1)
+	i := 0
+	for _, p := range h.providers {
+		ps[i] = p
+		i++
+	}
+	ps[len(ps)-1] = h.global
+
+	for _, p := range ps {
+		if err := p.initExceptions(); err != nil {
+			return err
+		}
+		if err := p.initRawRules(); err != nil {
+			return err
+		}
+		if err := p.initRawRules(); err != nil {
+			return err
 		}
 	}
 
-	return url, nil
+	return nil
+}
+
+func (h *Handler) Clear(rawURL string) (string, error) {
+	if rawURL == "" {
+		return "", nil
+	}
+
+	var provider *Provider
+	for _, p := range h.providers {
+		if p.urlPatternRegexp.MatchString(rawURL) {
+			provider = p
+			break
+		}
+	}
+
+	if provider == nil {
+		provider = h.global
+	}
+
+	if provider.CompleteProvider {
+		return rawURL, nil
+	}
+
+	if err := provider.initExceptions(); err != nil {
+		return "", err
+	}
+
+	for _, e := range provider.exceptionsRegexps {
+		if e.MatchString(rawURL) {
+			return rawURL, nil
+		}
+	}
+
+	if err := provider.initRawRules(); err != nil {
+		return "", err
+	}
+
+	for _, r := range provider.rawRulesRegexps {
+		rawURL = r.ReplaceAllString(rawURL, "")
+	}
+
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+
+	if err := provider.initRules(); err != nil {
+		return "", err
+	}
+
+	values := url.Values{}
+	for k, vs := range u.Query() {
+		remove := false
+		for _, rule := range provider.rulesRegexps {
+			if rule.MatchString(strings.ToLower(k)) {
+				remove = true
+				break
+			}
+		}
+		if !remove {
+			for _, v := range vs {
+				values.Add(k, v)
+			}
+		}
+	}
+
+	u.RawQuery = values.Encode()
+	return u.String(), nil
 }
--- cmd/clearurl/main.go
+++ cmd/clearurl/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/intakeapp/clearurl"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatalln("a parameter is required!")
+	}
+
+	h, err := clearurl.Init()
+	fatalIfErr(err)
+	var result []string
+	for _, u := range strings.Split(args[0], "\n") {
+		r, err := h.Clear(u)
+		fatalIfErr(err)
+		result = append(result, r)
+	}
+	fmt.Println(strings.Join(result, "\n"))
+}
+
+func fatalIfErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
